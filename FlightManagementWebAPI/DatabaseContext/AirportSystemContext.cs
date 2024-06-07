1e1df99c0c660139e6c1896a53f28c944f9e22cd
mdodig <mdodig7@gmail.com>
2024-06-07 14:24:15 +0200

    Airport system

---
+++ FlightManagementWebAPI/DatabaseContext/AirportSystemContext.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightManagementWebAPI.DatabaseContext
+{
+    public class AirportSystemContext : DbContext
+    {
+        public AirportSystemContext(DbContextOptions<AirportSystemContext> options)
+            : base(options)
+        {
+
+        }
+
+        //tablica
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Carrier> Carriers { get; set; }
+    }
+}
